fa2e73a210a7a6a6771e1f0ec6bf1c7fe6c4734e
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2019-12-07 14:42:39 -0800

    Added Rani's code to html & .js

--- script.js
+++ script.js
@@ -2,45 +2,51 @@ $("#findCity").on("click", function(event) {
   event.preventDefault();
   searchForCityByName();
 });
-
+var service;
+var map;
 function searchForCityByName() {
   var city = $("#autocomplete-input").val();
-
   var APIKey = "089100f1dce99fc69ca132b28b1e31ea";
   var queryURLWeather =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&units=imperial&appid=" +
     APIKey;
-
   $.ajax({
     url: queryURLWeather,
     method: "GET"
   }).then(function(response) {
     $(".temp").text("Temperature (F) " + response.main.temp);
-    console.log();
     var lat = response.coord.lat;
     var lon = response.coord.lon;
-    console.log(lat);
-    console.log(lon);
-
-    myMap(lat, lon);
+    console.log("lat", lat);
+    console.log("long", lon);
+    map = myMap(lat, lon);
+    var request = {
+      query: city,
+      fields: ["name", "geometry"]
+    };
+    service = new google.maps.places.PlacesService(map);
+    service.findPlaceFromQuery(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+        map.setCenter(results[0].geometry.location);
+      }
+    });
   });
-
-  let googleAPIKey = "AIzaSyAwPtnt5c8q3p2D4R6axYe1PKhCTVkawcI";
-  let placeQuery = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + city + "+city+point+of+interest&language=en&key=" + googleAPIKey;
-
-
-  $.ajax({
-    url: placeQuery,
-    method: "GET"
-  }).then(function(response) {
-    console.log('google api response', response);
-  })
-};
-
-
-
+}
+function createMarker(place) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  google.maps.event.addListener(marker, "click", function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
 function myMap(lat, lon) {
   var mapProp = {
     center: new google.maps.LatLng(lat, lon),
@@ -48,9 +54,8 @@ function myMap(lat, lon) {
   };
   return new google.maps.Map(document.getElementById("map"), mapProp);
 }
-
-$('input').keypress(event => {
+$("input").keypress(event => {
   if (event.which == 13) {
     searchForCityByName();
   }
-})
+});
