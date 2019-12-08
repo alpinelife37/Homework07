e0022c168304be8d3d4345b8bb493ae6716eef58
Sydney Guitjens <sguitjens@gmail.com>
2019-12-08 15:02:48 -0800

    fix layout

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function searchForCityByName() {
     url: queryURLWeather,
     method: "GET"
   }).then(function(response) {
-    $(".temp").text("Temperature (F) " + response.main.temp);
+    $(".temp").text("Temperature " + response.main.temp + String.fromCharCode(176) + "F");
     var lat = response.coord.lat;
     var lon = response.coord.lon;
     console.log("lat", lat);
@@ -36,7 +36,7 @@ function searchForCityByName() {
         }
         map.setCenter(results[0].geometry.location);
       }
-
+      console.log("place service response", results);
       ///////////////////////////////////////////////////////////////////////////////
       var latLon = { lat: lat, lng: lon };
       service.nearbySearch(
